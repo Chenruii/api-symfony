994a09c5a49e538cef0e85d67eb4ffe60706c942
Chenruii <rui.chen1996@gmail.com>
2019-01-28 11:15:34 +0100

    Create first routes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Rest\Get("api/users/{email}")
      */
-    public function index()
+    public function  getUser(User $user)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+    }
+    /**
+     * @Rest\Get("api/users")
+     */
+    public function  getApiUsers()
+    {
+    }
+    /**
+     * @Rest\Get("api/users")
+     */
+    public function  getApiUser(User $user)
+    {
+    }
+    /**
+     * @Rest\Get("api/users/{email}")
+     */
+    public function  patchApiUser(User $user)
+    {
+    }
+    /**
+     * @Rest\Get("api/users/{email}")
+     */
+    public function  deleteApiUser(User $user)
+    {
     }
 }
