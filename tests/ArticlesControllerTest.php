e9880ca207b4023811642a806b8834d6f329cd08
Chenruii <rui.chen1996@gmail.com>
2019-01-30 21:55:39 +0100

    API Testing with PHPUnit 6 update

---
+++ tests/ArticlesControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 30/01/2019
+ * Time: 15:07
+ */
+
+namespace App\tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class ArticlesControllerTest extends WebTestCase
+{
+    public function testGetArticles()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/articles', [], [], ['HTTP_ACCEPT' => 'application/json']);
+
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($content);
+
+        $arrayContent = \json_decode($content, true);
+        $this->assertCount(10, $arrayContent);
+    }
+
+    public function testPostArticles()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/articles', [], [],
+            [
+                'HTTP_ACCEPT' => 'application/json',
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_X-AUTH-TOKEN' => 'aaa'
+            ],
+            '{"name": "lorem","description": "blablabla"}'
+
+        );
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertJson($content);
+    }
+
+
+}
