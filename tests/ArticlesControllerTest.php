312cb1169b010807e13863013537a71e475b6089
Chenruii <rui.chen1996@gmail.com>
2019-01-31 14:40:02 +0100

    test

--- tests/ArticlesControllerTest.php
+++ tests/ArticlesControllerTest.php
@@ -13,37 +13,37 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ArticlesControllerTest extends WebTestCase
 {
-    public function testGetArticles()
-    {
-        $client = static::createClient();
-        $client->request('GET', '/api/articles', [], [], ['HTTP_ACCEPT' => 'application/json']);
-
-        $response = $client->getResponse();
-        $content = $response->getContent();
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertJson($content);
-
-        $arrayContent = \json_decode($content, true);
-        $this->assertCount(10, $arrayContent);
-    }
-
-    public function testPostArticles()
-    {
-        $client = static::createClient();
-        $client->request('POST', '/api/articles', [], [],
-            [
-                'HTTP_ACCEPT' => 'application/json',
-                'CONTENT_TYPE' => 'application/json',
-                'HTTP_X-AUTH-TOKEN' => 'aaa'
-            ],
-            '{"name": "lorem","description": "blablabla","createAt":"2019-01-01"}'
-
-        );
-        $response = $client->getResponse();
-        $content = $response->getContent();
-
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertJson($content);
-    }
+//    public function testGetArticles()
+//    {
+//        $client = static::createClient();
+//        $client->request('GET', '/api/articles', [], [], ['HTTP_ACCEPT' => 'application/json']);
+//
+//        $response = $client->getResponse();
+//        $content = $response->getContent();
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertJson($content);
+//
+//        $arrayContent = \json_decode($content, true);
+//        $this->assertCount(10, $arrayContent);
+//    }
+//
+//    public function testPostArticles()
+//    {
+//        $client = static::createClient();
+//        $client->request('POST', '/api/articles', [], [],
+//            [
+//                'HTTP_ACCEPT' => 'application/json',
+//                'CONTENT_TYPE' => 'application/json',
+//                'HTTP_X-AUTH-TOKEN' => 'aaa'
+//            ],
+//            '{"name": "lorem","description": "blablabla","createAt":"2019-01-01"}'
+//
+//        );
+//        $response = $client->getResponse();
+//        $content = $response->getContent();
+//
+//        $this->assertEquals(500, $response->getStatusCode());
+//        $this->assertJson($content);
+//    }
 }
