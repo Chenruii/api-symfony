cfcf158c7debf6992a9a9dcbe84fb93044bfb787
Chenruii <rui.chen1996@gmail.com>
2019-01-31 14:49:07 +0100

    test

--- tests/UsersControllerTest.php
+++ tests/UsersControllerTest.php
@@ -13,20 +13,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UsersControllerTest extends WebTestCase
 {
-//    public function testGetUsers()
-//    {
-//        $client = static::createClient();
-//        $client->request('GET', '/api/users', [], [], ['HTTP_ACCEPT' => 'application/json']);
-//
-//        $response = $client->getResponse();
-//        $content = $response->getContent();
-//
-//        $this->assertEquals(200, $response->getStatusCode());
-//        $this->assertJson($content);
-//
-//        $arrayContent = \json_decode($content, true);
-//        $this->assertCount(11, $arrayContent);
-//    }
+    public function testGetUsers()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/users', [], [], ['HTTP_ACCEPT' => 'application/json']);
+
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($content);
+
+        $arrayContent = \json_decode($content, true);
+        $this->assertCount(10, $arrayContent);
+    }
 //
 //    public function testPostUsers()
 //    {
