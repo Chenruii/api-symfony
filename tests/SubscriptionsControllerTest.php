2746718a4da526f3622df4de661453189cdbad94
Chenruii <rui.chen1996@gmail.com>
2019-02-17 23:23:30 +0100

    test

--- tests/SubscriptionsControllerTest.php
+++ tests/SubscriptionsControllerTest.php
@@ -13,37 +13,39 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SubscriptionsControllerTest extends WebTestCase
 {
-//    public function testGetsubScriptions()
-//    {
-//        $client = static::createClient();
-//        $client->request('GET', '/api/subscriptions', [], [], ['HTTP_ACCEPT' => 'application/json']);
-//
-//        $response = $client->getResponse();
-//        $content = $response->getContent();
-//
-//        $this->assertEquals(200, $response->getStatusCode());
-//        $this->assertJson($content);
-//
-//        $arrayContent = \json_decode($content, true);
-//        $this->assertCount(10, $arrayContent);
-//    }
-//
-//    public function testPostsubScriptions()
-//    {
-//        $client = static::createClient();
-//        $client->request('POST', '/api/subscriptions', [], [],
-//            [
-//                'HTTP_ACCEPT' => 'application/json',
-//                'CONTENT_TYPE' => 'application/json',
-//                'HTTP_X-AUTH-TOKEN' => 'aaa'
-//            ],
-//            '{"name": "lorem","description": "blablabla","createAt":"2019-01-01"}'
-//
-//        );
-//        $response = $client->getResponse();
-//        $content = $response->getContent();
-//
-//        $this->assertEquals(500, $response->getStatusCode());
-//        $this->assertJson($content);
-//
+    public function testGetsubScriptions()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/subscriptions', [], [], ['HTTP_ACCEPT' => 'application/json']);
+
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($content);
+
+        $arrayContent = \json_decode($content, true);
+        $this->assertCount(10, $arrayContent);
+    }
+
+    public function testPostsubScriptions()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/subscriptions', [], [],
+            [
+                'HTTP_ACCEPT' => 'application/json',
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_X-AUTH-TOKEN' => 'aaa'
+            ],
+            '{"name": "lorem","description": "blablabla","createAt":"2019-01-01"}'
+
+        );
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertJson($content);
+
+
+    }
 }
