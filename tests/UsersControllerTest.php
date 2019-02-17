35419551a273b9288539634d15e92277303c3ae8
Chenruii <rui.chen1996@gmail.com>
2019-02-17 23:22:55 +0100

    test

--- tests/UsersControllerTest.php
+++ tests/UsersControllerTest.php
@@ -27,42 +27,42 @@ class UsersControllerTest extends WebTestCase
         $arrayContent = \json_decode($content, true);
         $this->assertCount(10, $arrayContent);
     }
-//
-//    public function testPostUsers()
-//    {
-//        $client = static::createClient();
-//        $client->request('POST', '/api/users', [], [],
-//            [
-//                'HTTP_ACCEPT' => 'application/json',
-//                'CONTENT_TYPE' => 'application/json',
-//                'HTTP_X-AUTH-TOKEN' => 'aaa'
-//            ],
-//            //'{"apiKey": "adqaaa","email": "test@behat.com"}'
-//            '{"apiKey": "teasting","email": "teasdaat@baehat.com"}'
-//        );
-//        $response = $client->getResponse();
-//        $content = $response->getContent();
-//
-//        $this->assertEquals(500, $response->getStatusCode());
-//        $this->assertJson($content);
-//    }
-//    public function testPatchUsers()
-//    {
-//        $client = static::createClient();
-//        $client->request('PATCH', '/api/users/{email}', [], [],
-//            [
-//                'HTTP_ACCEPT' => 'application/json',
-//                'CONTENT_TYPE' => 'application/json',
-//                'HTTP_X-AUTH-TOKEN' => 'aaa'
-//            ],
-//            '{"firstname": "llal","lastname": "jkjdkf","apiKey": "teasting","birthday": "2019-01-01"}'
-//        );
-//        $response = $client->getResponse();
-//        $content = $response->getContent();
-//
-//        $this->assertEquals(500, $response->getStatusCode());
-//        $this->assertJson($content);
-//    }
+
+    public function testPostUsers()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/users', [], [],
+            [
+                'HTTP_ACCEPT' => 'application/json',
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_X-AUTH-TOKEN' => 'aaa'
+            ],
+            //'{"apiKey": "adqaaa","email": "test@behat.com"}'
+            '{"apiKey": "teasting","email": "teasdaat@baehat.com"}'
+        );
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertJson($content);
+    }
+    public function testPatchUsers()
+    {
+        $client = static::createClient();
+        $client->request('PATCH', '/api/users/{email}', [], [],
+            [
+                'HTTP_ACCEPT' => 'application/json',
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_X-AUTH-TOKEN' => 'aaa'
+            ],
+            '{"firstname": "llal","lastname": "jkjdkf","apiKey": "teasting","birthday": "2019-01-01"}'
+        );
+        $response = $client->getResponse();
+        $content = $response->getContent();
+
+        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertJson($content);
+    }
 
 
 }
